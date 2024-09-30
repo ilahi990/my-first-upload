d3d92d136d6719f4a2f17ce0f12bc04a22684026
Areeb585 <141987972+Areeb585@users.noreply.github.com>
2024-09-30 03:47:55 -0700

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,1 +1,11 @@
+<div>
+    <!-- Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less. - Marie Curie -->
 
+            <!-- <label for="">{{$label}}</label>
+            <input type="{{$type}}" name="{{$name}}"> -->
+            <!-- <span>
+                @error('name')
+                {{$message}}
+                @enderror
+            </span> -->
+</div>
